04f287848a3e314382c6aeee9641b73dfb3cc6c4
CDX <80155611+CDX-1@users.noreply.github.com>
2025-07-25 15:42:36 -0400

    Removed unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
-import { Particles } from "@/components/magicui/particles";
 
 export default function Home() {
 	return (
